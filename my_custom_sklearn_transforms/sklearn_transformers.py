6efef113f822ec2baeb31c11170f06d67335cf78
DanteAlucard98 <34227437+DanteAlucard98@users.noreply.github.com>
2020-08-21 14:50:50 -0500

    Update sklearn_transformers.py

--- my_custom_sklearn_transforms/sklearn_transformers.py
+++ my_custom_sklearn_transforms/sklearn_transformers.py
@@ -14,3 +14,12 @@ class DropColumns(BaseEstimator, TransformerMixin):
         data = X.copy()
         # Retornamos um novo dataframe sem as colunas indesejadas
         return data.drop(labels=self.columns, axis='columns')
+    
+# Crear un objeto ``SimpleImputer``
+si = SimpleImputer(
+    missing_values=np.nan,  # los valores que faltan son del tipo ``np.nan`` (Pandas estándar)
+    strategy='constant',  # la estrategia elegida es cambiar el valor faltante por una constante
+    fill_value=0,  # la constante que se usará para completar los valores faltantes es un int64 = 0
+    verbose=0,
+    copy=True
+)
